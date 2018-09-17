7385ec4d2ec26947fea48dbee74f3c8bccb31b5f
Huang Xingqiao <a92304a92304@gmail.com>
2018-09-18 03:11:29 +0800

    將路線列表的item分離為組件。

---
+++ src/components/RouteListItem.vue
@@ -0,0 +1,71 @@
+<template lang="pug">
+.card.item.mb-2(:style='{ borderLeftColor: route.color, animationDelay: `${index * 0.05}s` }')
+  .card-body(@click.stop='gotoRoute(route.id)')
+    .row.align-items-center
+      .col-12.col-md-auto.text-left.text-md-center
+        .name {{ route.name.ch }}
+        span(v-if='route.name.hasOwnProperty(`en`)') {{ route.name.en }}
+      .col-12.col-md.mb-2
+        .departure
+          fa.mr-2(icon='map-marker' size="xs")
+          span {{ route.departure.ch }}
+          small(v-if='route.departure.hasOwnProperty(`en`)') {{ route.departure.en }}
+        .destination
+          fa.mr-2(icon='angle-right')
+          span {{ route.destination.ch }}
+          small(v-if='route.destination.hasOwnProperty(`en`)') {{ route.destination.en }}
+      .col-auto.direction-btn
+        button.btn.btn-primary(@click.stop='gotoRoute(route.id, `go`)') 去程
+        button.btn.btn-primary(@click.stop='gotoRoute(route.id, `back`)') 返程
+</template>
+
+<script>
+export default {
+  name: 'RouteListItem',
+  props:{
+    route: { type: Object, default: {} },
+    index: { type: Number , default: 0 },
+  },
+  methods: {
+    gotoRoute (id, direction = `go`) {
+      this.$router.push(`/display/${id}?direction=${direction}`)
+    }
+  }
+
+}
+</script>
+
+<style scoped lang="sass">
+@import "~bootswatch/dist/lux/variables"
+@import "~bootstrap/scss/bootstrap"
+@import "~bootswatch/dist/lux/bootswatch"
+
+
+.item
+  border-left: .5rem solid #bbb
+  text-align: left
+
+  .card-body
+    display: block
+    &:hover
+      background-color: $gray-300
+      text-decoration: none
+      cursor: pointer
+
+  .name
+    font-size: 2rem
+    font-weight: 900
+
+  .departure, .destination
+    color: $gray-700
+    font-size: 1.5rem
+    font-weight: bold
+    span
+      margin-right: .5rem
+
+  .direction-btn
+    button
+      border-right: solid white 1px
+      border-bottom: solid white 1px
+
+</style>
--- src/views/RouteList.vue
+++ src/views/RouteList.vue
@@ -1,41 +1,13 @@
 <template lang="pug">
 section
-  //- demo 用
   h4.text-light 演示路線
-  .card.item.mb-5
-    .card-body(@click.stop='gotoRoute(`demo`)')
-      .row.align-items-center
-        .col-auto.text-center #[.name {{ demoRoute.routeName.ch }}] #[span {{ demoRoute.routeName.en }}]
-        .col
-          .departure #[fa.mr-2(icon='map-marker' size="xs")] #[span {{ demoRoute.departureName.ch }}] #[small {{ demoRoute.departureName.en }}]
-          .destination #[fa.mr-2(icon='angle-right')] #[span {{ demoRoute.destinationName.ch }}] #[small {{ demoRoute.destinationName.en }}]
-        .col-auto
-          .btn-group
-            button.btn.btn-primary.border-right(@click.stop='gotoRoute(`demo`, `go`)') 去程
-            button.btn.btn-primary(@click.stop='gotoRoute(`demo`, `back`)') 返程
+  RouteListItem(:route='demoRoute' v-if='demoRoute')
 
   h4.text-light 線上路線列表
   .loading(v-if='!routes') #[fa(icon='spinner' spin)]
   transition-group(name='fade')
-    .card.item.mb-2(v-for='(route, index) in routes' v-if='routes' :key='index' :style='{ borderLeftColor: route.color, animationDelay: `${index * 0.05}s` }')
-      .card-body(@click.stop='gotoRoute(route.id)')
-        .row.align-items-center
-          .col-auto.text-center
-            .name {{ route.name.ch }}
-            span(v-if='route.name.hasOwnProperty(`en`)') {{ route.name.en }}
-          .col
-            .departure
-              fa.mr-2(icon='map-marker' size="xs")
-              span {{ route.departure.ch }}
-              small(v-if='route.departure.hasOwnProperty(`en`)') {{ route.departure.en }}
-            .destination
-              fa.mr-2(icon='angle-right')
-              span {{ route.destination.ch }}
-              small(v-if='route.destination.hasOwnProperty(`en`)') {{ route.destination.en }}
-          .col-auto
-            .btn-group
-              button.btn.btn-primary.border-right(@click.stop='gotoRoute(route.id, `go`)') 去程
-              button.btn.btn-primary(@click.stop='gotoRoute(route.id, `back`)') 返程
+    RouteListItem(v-for='(route, index) in routes' :route='route' :index='index' :key='index')
+
   .text-center(v-if='routes')
     button.btn.btn-outline-secondary.my-3(@click='gotoTop()')
       fa.mx-5(icon='caret-up' size="2x")
@@ -43,6 +15,7 @@ section
 
 <script>
 import $ from 'jquery'
+import RouteListItem from '@/components/RouteListItem'
 import demoRoute from '@/assets/js/demoRoute'
 
 export default {
@@ -50,11 +23,13 @@ export default {
   data () {
     return {
       routes: null,
-      demoRoute: demoRoute.route
+      demoRoute: null,
     }
   },
   mounted () {
+    this.setDemoRoute()
     this.fetchList()
+
     $('html').css('font-size', `14px`)
   },
   methods: {
@@ -62,13 +37,23 @@ export default {
       const url = `https://busplay-server.herokuapp.com/AllRouteXQ/`
       $.get(url, list => this.routes = list )
     },
-    gotoRoute (id, direction = `go`) {
-      this.$router.push(`/display/${id}?direction=${direction}`)
-    },
     gotoTop () {
       document.body.scrollTop = 0
       document.documentElement.scrollTop = 0
+    },
+    setDemoRoute () {
+      const demo = demoRoute.route
+      this.demoRoute = {
+        name: demo.routeName,
+        departure: demo.departureName,
+        destination: demo.destinationName,
+        id: `demo`,
+        color: ``,
+      }
     }
+  },
+  components: {
+    RouteListItem
   }
 }
 </script>
@@ -83,31 +68,6 @@ export default {
   font-size: 2rem
   text-align: center
 
-.item
-  border-left: .5rem solid #bbb
-  text-align: left
-
-  .card-body
-    display: block
-    &:hover
-      background-color: $gray-300
-      text-decoration: none
-      cursor: pointer
-
-  .name
-    font-size: 2rem
-    font-weight: 900
-
-  .departure, .destination
-    color: $gray-700
-    font-size: 1.5rem
-    font-weight: bold
-    span
-      margin-right: .5rem
-
-  .play
-    font-size: 1.5rem
-    color: $green
 
 @keyframes fade
   from
